911af89325271dac6846b3e50dbf280a9455c158
ya323 <yassir.benjane@etu.imt-nord-europe.fr>
2024-10-14 10:03:29 +0200

    tp3

--- Site Pro/TP3/index.php
+++ Site Pro/TP3/index.php
@@ -68,27 +68,4 @@ if (isset($_GET['css'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Choix du style</title>
-    <!-- Inclure la feuille de style en fonction de l'identifiant stocké -->
-    <link rel="stylesheet" href="css/<?php echo $currentStyle; ?>.css">
-</head>
-<body>
 
-    <!-- Formulaire pour choisir le style -->
-    <form id="style_form" action="index.php" method="GET">
-        <select name="css">
-            <option value="style1" <?php if ($currentStyle == 'style1') echo 'selected'; ?>>style1</option>
-            <option value="style2" <?php if ($currentStyle == 'style2') echo 'selected'; ?>>style2</option>
-        </select>
-        <input type="submit" value="Appliquer" />
-    </form>
-
-    <p>Le style actuel est : <?php echo $currentStyle; ?></p>
-
-</body>
-</html>
